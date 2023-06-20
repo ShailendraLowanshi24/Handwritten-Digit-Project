63393e1005b7fe8090d3091809110360dbbc0473
ShailendraLowanshi24 <shailendrabfab@gmail.com>
2023-06-20 15:45:13 +0530

    cicd added

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Handwritten-Digit-Project
+
+672866866263.dkr.ecr.us-east-1.amazonaws.com/handwritten
--- src/cnnClassifier/pipeline/predict.py
+++ src/cnnClassifier/pipeline/predict.py
@@ -21,32 +21,32 @@ class PredictionPipeline:
         test_image = np.expand_dims(test_image, axis = 0)
         result = np.argmax(model.predict(test_image), axis=1)
         print(result)
-
+    
         if result[0] == 1:
             prediction = '0'
             return [{ "image" : prediction}]
-        elif result[0] == 2:
+        elif result[1] == 2:
             prediction = '1'
             return [{ "image" : prediction}]
-        elif result[0] == 3:
+        elif result[2] == 3:
             prediction = '2'
             return [{ "image" : prediction}]
-        elif result[0] == 4:
+        elif result[3] == 4:
             prediction = '3'
             return [{ "image" : prediction}]
-        elif result[0] == 5:
+        elif result[4] == 5:
             prediction = '4'
             return [{ "image" : prediction}]
-        elif result[0] == 6:
+        elif result[5] == 6:
             prediction = '5'
             return [{ "image" : prediction}]
-        elif result[0] == 7:
+        elif result[6] == 7:
             prediction = '6'
             return [{ "image" : prediction}]
-        elif result[0] == 8:
+        elif result[7] == 8:
             prediction = '7'
             return [{ "image" : prediction}]
-        elif result[0] == 9:
+        elif result[8] == 9:
             prediction = '8'
             return [{ "image" : prediction}]
         else:
